d439c8d144072f0101e24b045d0c36373cd13688
Ritika Singh <singhrit@usc.edu>
2023-04-12 05:44:35 +0000

    done!

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,3 +1,4 @@
+
 #ifndef RECCHECK
 #include <set>
 #include <iostream>
@@ -19,7 +20,18 @@ using namespace std;
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
+
+
 // Add prototypes for any helper functions here
+bool backtrack(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, 
+		map<size_t, size_t>& workerShifts, 
+		int scheduling,
+    const size_t day
+);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -36,9 +48,56 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+		sched.resize(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID));
+
+		for (unsigned int i = 0; i < avail.size(); i++){
+			sched[i].resize(dailyNeed, -1);
+		}
+
+		map<size_t, size_t> workerShifts;
+		for (size_t w = 0; w < avail[0].size(); ++w){
+			workerShifts.insert(make_pair(w, 0));
+		}
 
 
-
+		return backtrack(avail, dailyNeed, maxShifts, sched, workerShifts, 0, 0);
 
 }
+
+bool backtrack(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, 
+		map<size_t, size_t>& workerShifts, 
+		int scheduling,
+    const size_t day
+){
+    if (day == avail.size()){
+        return true;
+    }
+
+    if (scheduling == static_cast<int>(dailyNeed)){
+			return backtrack(avail, dailyNeed, maxShifts, sched, workerShifts, 0, day + 1);
+		}
+		else{
+			for (Worker_T w = 0; w < avail[day].size(); ++w){
+				if (avail[day][w] == 1 && find(sched[day].begin(), sched[day].end(), w) == sched[day].end() && 
+				workerShifts.find(w) -> second < maxShifts){
+					sched[day][scheduling] = w;
+					workerShifts.find(w) -> second++;
+					if (backtrack(avail, dailyNeed, maxShifts, sched, workerShifts, scheduling + 1, day)){
+						return true;
+					}
+					else{
+						sched[day][scheduling] = -1;
+						workerShifts.find(w) -> second--;
+					}
+				}
+			}
+		}
+    return false;
+}
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,14 +5,24 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
 
+// data structure like a map that keeps track of used floating letters 
+// multi set?
+// for all floating letters you start out with, every time you insert one then insert into used floating set multi set
+
 
 // Add prototypes of helper functions here
+// checks if word is valid given set of dictionary words
+
+void getCombinations(const std::string& pattern, const string& floating, std::set<std::string>& results, int index, const std::set<std::string>& dict);
+bool isValid(const std::string& word, const std::set<std::string>& dict);
+int countChars(const std::string& str);
 
 
 // Definition of primary wordle function
@@ -21,8 +31,86 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+	std::set<std::string> results;
+
+	//std::string word(in.size(), ' ');
+
+	getCombinations(in, floating, results, 0, dict);
+
+	return results;
 
 }
+//map<char, int> floating;
+// map of character to integer where integer is count of that character in the floating string
 
-// Define any helper functions here
+void getCombinations(const std::string& pattern, const string& floating, std::set<std::string>& results, int index, const std::set<std::string>& dict){
+
+	// in base case, don't need to loop through floating to see if matches character
+	// just check if floating map is empty
+	// if floating map is empty, then word is valid
+	string f = floating;
+	string word = pattern;
+	
+	if (f.size() == 0 && index == static_cast<int>(word.size())){
+		if (isValid(word, dict)){
+			results.insert(pattern);
+		}
+		return;
+	}
+	unsigned int spaces = countChars(word);
+	
+	if (word[index] == '-'){
+		if (f.size() == spaces){
+			for (unsigned int i = 0; i < f.size(); i++){
+				word[index] = f[i];
+				f.erase(i,1);
+				getCombinations(word, f, results, index + 1, dict);
+				f = floating;
+			}
+		}
+		else if (spaces > f.size()){
+			for (char c = 'a'; c <= 'z'; c++){
+				word[index] = c;
+				if (f.find(c) != string::npos){
+					f.erase(f.find(c),1);
+					getCombinations(word, f, results, index+1, dict);
+					f = floating;
+				}
+				else{
+					getCombinations(word, f, results, index+1, dict);
+				}
+			}
+		}
+		else{
+			return;
+		}
+	}
+	else{
+		getCombinations(word, f, results, index+1, dict);
+	}
+	return;
+}
+
+
+
+bool isValid(const std::string& word, const std::set<std::string>& dict){
+	if (dict.find(word) != dict.end()){
+		return true;
+	}
+	return false;
+}
+
+
+
+int countChars(const std::string& str){
+	int count = 0;
+	for (char ch: str){
+		if (ch == '-'){
+			count++;
+		}
+	}
+	return count;
+}
+
+
+
